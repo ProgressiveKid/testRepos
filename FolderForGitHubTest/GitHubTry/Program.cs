4c6577647a710fae9955491f8c9d6e4278c00b0a
Vlad <jostonn@mail.ru>
2023-12-18 15:01:44 +0300

    Второй пуш реквест из бэка

--- FolderForGitHubTest/GitHubTry/Program.cs
+++ FolderForGitHubTest/GitHubTry/Program.cs
@@ -5,10 +5,10 @@
 
 
         // Я тут написал такой бэк: тут и бд, и кэширование, и логирование и всё всё всё
-        //
+        // Я переделал бэк
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, из ветки backeEnd");
+            Console.WriteLine("Hello, из ветки backeEnd, я пофиксил то что было поломано");
         }
     }
 }
