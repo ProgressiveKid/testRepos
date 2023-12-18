d13a5485df6c5726f7eee8bb4c8c8a6830a069c9
Vlad <jostonn@mail.ru>
2023-12-18 14:57:29 +0300

    Привет от BackEnda

--- FolderForGitHubTest/GitHubTry/Program.cs
+++ FolderForGitHubTest/GitHubTry/Program.cs
@@ -2,9 +2,13 @@
 {
     internal class Program
     {
+
+
+        // Я тут написал такой бэк: тут и бд, и кэширование, и логирование и всё всё всё
+        //
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, из ветки backeEnd");
         }
     }
 }
