2a5824d82d597f5a27dbdd9f11c902d787484dbb
Vlad <jostonn@mail.ru>
2023-12-19 14:10:11 +0300

    Коммит из фронта

--- FolderForGitHubTest/GitHubTry/Program.cs
+++ FolderForGitHubTest/GitHubTry/Program.cs
@@ -2,9 +2,10 @@
 {
     internal class Program
     {
+        // комментарии со стороны фронта
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, привет из фронта");
         }
     }
 }
